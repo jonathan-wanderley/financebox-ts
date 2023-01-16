3e3362af4e424b6a9193285c46b9db9f1f0faa8d
Jonathan Wanderley <jonathan.wpc@gmail.com>
2023-01-16 13:56:36 -0300

    feat: add a transaction model and migration

---
+++ prisma/migrations/20230116165524_create_transaction_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "value" DECIMAL(65,30) NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
